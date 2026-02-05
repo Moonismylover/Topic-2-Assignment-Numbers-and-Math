302a2e90a96ad80e4bc2be83c8aae9f1dde719e9
700014473 <700014473@2260I-L29-008>
2026-02-05 13:06:14 -0500

    Add project files.

---
+++ Topic 2 - Assignment (Numbers and Math)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_2___Assignment__Numbers_and_Math_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
