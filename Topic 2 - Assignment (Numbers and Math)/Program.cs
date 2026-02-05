c5145339db6a621f252badf901d22609279208cb
700014473 <700014473@2260I-L29-008>
2026-02-05 13:17:51 -0500

    Sup

--- Topic 2 - Assignment (Numbers and Math)/Program.cs
+++ Topic 2 - Assignment (Numbers and Math)/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Angel
+
+            Console.Title = "Topic 2 - Assignment (Numbers and Math)";
+
+            Console.WriteLine("I was born in 2009. As of right now, I am " + (2026 - 2009) + " years old.");
+
+            Console.WriteLine("The average of the numbers '14, 15, 16' is " + ((14 + 15 + 16) / 3) + ".");
+
+            Console.WriteLine("The average of the numbers '14, 15, 16, 17' is " + ((14.0 + 15.0 + 16.0 + 17.0) / 4.0) + ".");
+
+
+
+
         }
     }
 }
